22ab03bf661d7befb892df4cc769db285e5695d1
Megan <mschmi26@nd.edu>
2021-10-13 18:39:10 -0400

    results

--- bioinformaticsProject/ref_sequences/sorting.sh
+++ bioinformaticsProject/ref_sequences/sorting.sh
@@ -10,19 +10,25 @@ do
 	
 done	
 
-cat mrcAproteome_*.fasta
+
 for k in mrcAproteome_*.fasta
 do 	
-cat $k | grep "WP" | sed -e s/'\s'/,/g 
-#echo $k " " $number >> t.txt
+	number=$(cat $k | grep "WP" | xargs | cut -d " " -f 6) 
+	echo $k " " $number >> t.txt
 done	
-#done 
-#for z in hsp70gene_*.fasta
-#do
-#	./muscle -in $z -out muscle$z
-#	./hmmbuild build$z muslce$z
-#for l in hasmrcA*.fasta
-#do
+cat t.txt | sort -r -k 2 | grep -E '[0-9]{3,}' > hasmcrAgene.txt 
+ 
+cat hsp70gene_*.fasta >> allhsp.fasta
+	./muscle -in allhsp.fasta -out muscleallhsp.fasta
+	./hmmbuild buildallhsp.fasta muscleallhsp.fasta
+for l in proteome_*.fasta
+do
+ 	./hmmsearch --tblout hsp$l buildallhsp.fasta $l
+done
+for b in hspproteome_*.fasta
+do
+amount=$(cat $b | grep -c "WP" )
+echo $b " " $amount >> s.txt
+done
+cat s.txt | sort -r -k 2 > hashspgene.txt
 
-#done
-#done
