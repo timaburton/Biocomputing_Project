b3b11698f1332aa574d37d9f21c488843f9e0632
Casey Novak <cnovak@crcfe01.crc.nd.edu>
2021-10-13 18:04:25 -0400

    For loops are working now and code runs from bashproject directory

--- bioinformaticsProject/BioProject.sh
+++ bioinformaticsProject/BioProject.sh
@@ -1,27 +1,31 @@
-#combine reference sequence files for mcrAgene and  hsp70gene
+# Usage: bash BioProject.sh
+# must be used in bioinformatics folder for relative paths to work, should we change this so that it can be used from anywhere? 
+# Path to bioinformatics folder: /afs/crc.nd.edu/user/c/cnovak/Private/bashproject/Biocomputing_Project/bioinformaticsProject
 
-cat hsp70gene_*.fasta >> hsp70gene_combined_file.fasta
-cat mcrAgene_*.fasta >> mcrAgene_combined_file.fasta 
+# Combine reference sequence files for mcrAgene
+
+#cat hsp70gene_*.fasta >> hsp70gene_combined_file.fasta
+cat ./ref_sequences/mcrAgene_*.fasta >> mcrAgene_combined_file1.fasta 
 
 # Align mcrA reference sequences using muscle and build an hmm profile using hmm build
 
-../../../muscle3.8.31_i86linux64 -in mcrAgene_combined_file.fasta -out mcrA_muscle_results
-~/Private/bin/bin/hmmbuild mcrA_hmmbuild_results mcrA_muscle_results
+~/Private/bashproject/muscle3.8.31_i86linux64 -in mcrAgene_combined_file1.fasta -out mcrA_muscle_results1
+~/Private/bin/bin/hmmbuild mcrA_hmmbuild_results1 mcrA_muscle_results1
 
 # Search proteomes using hmm profile for mcrA gene
 
-for proteome in proteomes
-do
-~/Private/bin/bin/hmmsearch --tblout mcrA_search_results ./ref_sequences/mcrA_hmmbuild_results ./proteomes/*.fasta >> proteome_mcrA_results 
-done
+for proteome in {01..50}; do ~/Private/bin/bin/hmmsearch --tblout mcrA_hmmsearch_results_proteome$proteome mcrA_hmmbuild_results1 ./proteomes/proteome_$proteome.fasta; done
 
-#Worked for one file, need to turn into loop
+# Combine reference sequence files for hsp70gene
 
-~/Private/bin/bin/hmmsearch --tblout mcrA_hmmsearch_results_proteome01 ./ref_sequences/mcrA_hmmbuild_results ./proteomes/proteome_01.fasta
+cat ./ref_sequences/hsp70gene_*.fasta >> hsp70gene_combined_file1.fasta
 
+# Align hsp70 reference sequences using muscle and build an hmm profile using hmm build
 
+#~/Private/bashproject/muscle3.8.31_i86linux64 -in hsp70gene_combined_file1.fasta -out hsp70_muscle_results1
+#~/Private/bin/bin/hmmbuild hsp70_hmmbuild_results1 hsp70_muscle_results1
 
-# ALigns hsp70 reference sequences using muscle and build an hmm profile using hmm build
+# Search proteomes using hmm profile for hsp70 gene
 
-../../../muscle3.8.31_i86linux64 -in hsp70gene_combined_file.fasta -out hsp70_muscle_results
-~/Private/bin/bin/hmmbuild hsp70_hmmbuild_results hsp70_muscle_results
+for proteome in {01..50}; do ~/Private/bin/bin/hmmsearch --tblout hsp70_hmmsearch_results_proteome$proteome hsp70_hmmbuild_results1 ./proteomes/proteome_$proteome.fasta; done
+
