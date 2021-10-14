2ca0aadfc2d3358c2afa246de87e406d3bf33bb3
Casey Novak <cnovak@crcfe01.crc.nd.edu>
2021-10-13 20:03:03 -0400

    Combined loops, created a table with results, need to add headers to table

--- bioinformaticsProject/BioProject.sh
+++ bioinformaticsProject/BioProject.sh
@@ -13,8 +13,7 @@ cat ./ref_sequences/mcrAgene_*.fasta >> mcrAgene_combined_file1.fasta
 ~/Private/bin/bin/hmmbuild mcrA_hmmbuild_results1 mcrA_muscle_results1
 
 # Search proteomes using hmm profile for mcrA gene
-
-for proteome in {01..50}; do ~/Private/bin/bin/hmmsearch --tblout mcrA_hmmsearch_results_proteome$proteome mcrA_hmmbuild_results1 ./proteomes/proteome_$proteome.fasta; done
+#for proteome in {01..50}; do ~/Private/bin/bin/hmmsearch --tblout mcrA_hmmsearch_results_proteome$proteome mcrA_hmmbuild_results1 ./proteomes/proteome_$proteome.fasta; done
 
 # Combine reference sequence files for hsp70gene
 
@@ -25,7 +24,18 @@ cat ./ref_sequences/hsp70gene_*.fasta >> hsp70gene_combined_file1.fasta
 ~/Private/bashproject/muscle3.8.31_i86linux64 -in hsp70gene_combined_file1.fasta -out hsp70_muscle_results1
 ~/Private/bin/bin/hmmbuild hsp70_hmmbuild_results1 hsp70_muscle_results1
 
+# Create output file
+Echo 'Proteome_number, mcrA_matches, hsp70_matches' > summary_table.csv
+
 # Search proteomes using hmm profile for hsp70 gene
 
-for proteome in {01..50}; do ~/Private/bin/bin/hmmsearch --tblout hsp70_hmmsearch_results_proteome$proteome hsp70_hmmbuild_results1 ./proteomes/proteome_$proteome.fasta; done
+for proteome in {01..50}
+do
+~/Private/bin/bin/hmmsearch --tblout hsp70_hmmsearch_results_proteome$proteome hsp70_hmmbuild_results1 ./proteomes/proteome_$proteome.fasta 
+hsp70matches=$(cat hsp70_hmmsearch_results_proteome$proteome | grep -E -v "#" | wc -l)
+proteome_name=proteome$proteome
+~/Private/bin/bin/hmmsearch --tblout mcrA_hmmsearch_results_proteome$proteome mcrA_hmmbuild_results1 ./proteomes/proteome_$proteome.fasta
+mcrAmatches=$(cat mcrA_hmmsearch_results_proteome$proteome | grep -E -v "#" | wc -l)
+echo $proteome_name, $mcrAmatches, $hsp70matches >> summary_table.csv
+done
 
