6ecd5b2c12df4eee76934814d69ae9e33d0cc2e7
Zachary Tabellione <ztabelli@crcfe02.crc.nd.edu>
2021-10-13 21:04:26 -0400

    second installment

--- bioinformaticsProject/ref_sequences/biocomp_project.sh
+++ bioinformaticsProject/ref_sequences/biocomp_project.sh
@@ -1,7 +1,9 @@
 #this is the rough draft of our Bash project script
-#usage: TBD but something around bash biocomp_project.sh <rawfiledir> <proteomsdir>
+#usage: TBD but something around bash biocomp_project.sh <ref sequence file> <location of muscle tool>
 
+#access ref file 
 
+cd $1
 
 #to combine the raw hsp70 files into one fasta that we will hmm
 
@@ -10,3 +12,10 @@ cat hsp70* | grep -v fasta > hsp70.fasta
 #to combine the raw mcrAgene files into one fasta that we will hmm
 
 cat mcrAgene* | grep -v fasta > mrcAgene.fasta
+
+#muscling the compiled hsp70
+
+$2 -in hsp70.fasta -out hsp70.hmm
+
+#muscling the compiled mcrA
+$2 -in hsp70.fasta -out hsp70.hmm
